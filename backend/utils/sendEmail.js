14c8f05e034b811b41a9ad7541c19a3f6c8fdb36
jayaditya30 <177267310+jayaditya77@users.noreply.github.com>
2026-05-28 21:48:16 +0530

    fix gmail tls

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -8,9 +8,15 @@ const transporter = nodemailer.createTransport({
 
   secure: false,
 
+  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+
+  tls: {
+    rejectUnauthorized: false
   }
 
 });
